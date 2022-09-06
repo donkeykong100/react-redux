afb282cbf0a0d27c663eeebbb888ccd3eaef786c
donkey_kong <donghui8266@gmail.com>
2022-09-06 13:02:27 +0900

    Redux 사용해서 Vanilla counter 구현

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
+import { legacy_createStore } from "redux";
+
 const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
+const PLUS = "Plus";
+const MINUS = "Minus";
 
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  // Reducer는 Application의 데이터인 state와
+  // 할 일을 정의하는 action을 포함한다.
+  // Reducer가 return하는 것은 무엇이든지 Application의 state가 된다.
+  switch (action.type) {
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
-const handlePlus = () => {
-  count += 1; // number.innerText = number.innerText + 1;
-  updateText();
+const countStore = legacy_createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 };
 
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
+countStore.subscribe(onChange);
 
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+plus.addEventListener("click", () => countStore.dispatch({ type: PLUS }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
