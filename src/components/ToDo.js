c6801e4a82ac5b40c6c5931c7e130e1f2ca48616
donkey_kong <donghui8266@gmail.com>
2022-09-09 21:19:04 +0900

    Detail Page 구현

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../routes/store";
+import { Link } from "react-router-dom";
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ text, onBtnClick, id }) => {
   return (
     <li>
-      {text}
+      <Link to={`/${id}`}>{text}</Link>
       <button onClick={onBtnClick}>DEL</button>
     </li>
   );
