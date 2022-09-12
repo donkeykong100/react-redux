7a08de1e2e883e9b6e1e90852661419a34593add
donkey_kong <donghui8266@gmail.com>
2022-09-13 01:56:50 +0900

    redux toolkit을 이용해서 구현

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,34 +1,30 @@
-import { legacy_createStore } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((element) => element.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+    // 새로운 배열을 반환하지 않고 기존 배열을 변형하는 경우 return문을 사용하지 않음
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((element) => element.id !== action.payload),
+  // 새로운 배열을 반환하기 때문에 return문 사용(화살표 함수 한 줄 표현식 return문 생략)
+});
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((element) => element.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = legacy_createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
